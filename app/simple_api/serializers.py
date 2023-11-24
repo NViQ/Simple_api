88403eb96afa2fce483a3e9973e6046955401bca
Павел <naveq4358@gmail.com>
2023-11-25 00:28:21 +0300

    Commit

--- app/simple_api/serializers.py
+++ app/simple_api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Log
 
+
 class LogSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
 
